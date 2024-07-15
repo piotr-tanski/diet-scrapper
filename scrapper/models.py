a4090a5bc1be06a77f17841e1bfd171271bfcfe1
Piotr Tański <pit@spyro-soft.com>
2024-07-15 20:41:36 +0200

    Improvements to the selenium driver

--- scrapper/models.py
+++ scrapper/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,12 +15,12 @@ def get_diet(diet):
         result = _get_maczfit()
 
     end = time.time()
-    print(f"get_diet({diet}) took {end - start}")
+    print(f"get_diet({diet}) took {end - start} seconds")
     return result
 
 
 def _get_maczfit():
-    driver = webdriver.Chrome()
+    driver = _get_chrome_driver()
     diets = ['comfort', 'vege', 'fit', 'wegan', 'fodmap', 'no-lactose-&-low-gluten', 'diabetic', 'hypo-hashimoto', 'keto']
     meals = defaultdict(list)
     for diet in diets:
@@ -37,3 +36,12 @@ def _get_maczfit():
             if name and desc:
                 meals[name].append(desc)
     return meals
+
+
+def _get_chrome_driver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    return webdriver.Chrome(chrome_options=chrome_options)
