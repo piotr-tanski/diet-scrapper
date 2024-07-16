76fea0eb68abe62ccc576fbf8675fbc2fb44ebce
Piotr Tański <pit@spyro-soft.com>
2024-07-16 09:29:25 +0200

    Handle exceptions

--- scrapper/models.py
+++ scrapper/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -24,17 +25,17 @@ def _get_maczfit():
     driver = _get_chrome_driver()
     for diet in ['comfort', 'vege', 'fit', 'fodmap', 'no-lactose-&-low-gluten', 'diabetic', 'hypo-hashimoto']:
         print(f"Getting {diet} from maczfit")
-
         driver.get(f'https://www.maczfit.pl/programy-maczfit?diet={diet}&active=0')
-        element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'menu__meals')))
-        if not element:
-            continue
-
-        for e in driver.find_elements(By.CLASS_NAME, 'menu__meals'):
-            name = e.find_element(By.CLASS_NAME, 'meal__name').text
-            desc = e.find_element(By.CLASS_NAME, 'meal__description').text
-            if name and desc:
-                meals[name].append(desc)
+        try:
+            element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'menu__meals')))
+            for e in driver.find_elements(By.CLASS_NAME, 'menu__meals'):
+                name = e.find_element(By.CLASS_NAME, 'meal__name').text
+                desc = e.find_element(By.CLASS_NAME, 'meal__description').text
+                if name and desc:
+                    meals[name].append(desc)
+        except TimeoutException:
+            print(f"Couldn't get {diet} from maczfit!")
+            pass
     return meals
 
 
