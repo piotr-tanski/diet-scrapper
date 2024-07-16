3ab323d7ccf7ea25e3a81a9f1ce4abe53d6fbabe
Piotr Tański <pit@spyro-soft.com>
2024-07-16 09:57:58 +0200

    Fixes for the docker for arm64.

--- scrapper/views.py
+++ scrapper/views.py
@@ -16,22 +16,21 @@ def index(request):
 <body>
 
 <h1>Diet Scrapper</h1>
-<pre id="maczfit"></pre>
+<pre id="maczfit">Loading...</pre>
 
 <script>
 function loadDiet(diet) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.LOADING) {
-        document.getElementById(diet).innerHTML = "Loading..."
-    } else if (this.readyState == XMLHttpRequest.DONE) {
-        if (this.status === 0 || (this.status >= 200 && this.status < 400)) {
-          document.getElementById(diet).innerHTML = JSON.stringify(JSON.parse(this.responseText), null, 2);
-        } else if (this.status >= 400 && this.status < 500) {
-          document.getElementById(diet).innerHTML = "Could not find!"
-        } else if (this.status >= 500) {
-          document.getElementById(diet).innerHTML = "Error occurred! Try again later."
-        }
+    console.log("XMLHttpRequest state changed:", this.readyState, this.status)
+    if (this.readyState == XMLHttpRequest.DONE) {
+      if (this.status === 0 || (this.status >= 200 && this.status < 400)) {
+        document.getElementById(diet).innerHTML = JSON.stringify(JSON.parse(this.responseText), null, 2);
+      } else if (this.status >= 400 && this.status < 500) {
+        document.getElementById(diet).innerHTML = "Could not find!"
+      } else if (this.status >= 500) {
+        document.getElementById(diet).innerHTML = "Error occurred! Try again later."
+      }
     }
   };
   xhttp.open("GET", "diet?diet=" + diet, true);
